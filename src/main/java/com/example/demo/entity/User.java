32acb265488809aac434d8f01ea2a7a2f61269f2
rohitpandeyoo6 <98679341+rohitpandeyoo6@users.noreply.github.com>
2023-03-08 02:08:22 +0530

    first commit 08-03-23

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends AbstractEntity {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String passowrd;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassowrd() {
+		return passowrd;
+	}
+
+	public void setPassowrd(String passowrd) {
+		this.passowrd = passowrd;
+	}
+
+}
